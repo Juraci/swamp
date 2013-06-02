7ed3b826f74ddac0516ca50747ecf9fc7b1aa3df
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2013-06-02 15:51:58 -0300

    handling invalid url.

--- lib/swamp/interface.rb
+++ lib/swamp/interface.rb
@@ -1,28 +1,36 @@
 module Swamp
-  class Interface
+	class Interface
 
-    MESSAGE = 'Enter the url for the page to be scanned:'
+		MESSAGE = 'Enter the url for the page to be scanned:'
+		WARNING_MESSAGE = 'Please enter a valid url!'
 
-    def initialize(output, wrapper)
-      @output = output
-      @wrapper = wrapper
-    end
+		def initialize(output, wrapper)
+			@output = output
+			@wrapper = wrapper
+		end
 
-    def run
-      @output.puts(MESSAGE)
-    end
+		def run
+			@output.puts(MESSAGE)
+		end
 
-    def scan(url)
-      @wrapper.explore(url)
-      snippets = @wrapper.scan
-      present(snippets)
-    end
+		def scan(url)
+			if is_valid_url?(url)
+				@wrapper.explore(url)
+				snippets = @wrapper.scan
+				present(snippets)
+			else
+				@output.puts(WARNING_MESSAGE)
+			end
+		end
 
-    def present(snippets)
-      snippets.each do |snippet|
-        @output.puts(snippet) unless snippet == nil
-      end
-    end
-  end
+		def is_valid_url?(url)
+			url.match(/^(http|https|file):\/\/.*$/i) ? true : false
+		end
+
+		def present(snippets)
+			snippets.each do |snippet|
+				@output.puts(snippet) unless snippet == nil
+			end
+		end
+	end
 end
-
