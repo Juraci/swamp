675564a0618f4118f5e03f2cf522d7c7226728e0
Juraci Vieira <juraci.vieira@gmail.com>
2015-12-15 11:55:32 -0200

    refactoring jasmine tests

--- spec/highlightSpec.js
+++ spec/highlightSpec.js
@@ -2,17 +2,17 @@ describe('Highlight', function (){
     var HIGHLIGHT = require('../lib/swamp/js-modules/highlight.js');
     var element;
 
-    it('should set the boxShadow property of the element', function (){
-        element = { style: { boxShadow: '' } };
+    beforeEach(function (){
+        element = { style: {} };
         spyOn(element, 'style');
         HIGHLIGHT.glow(element);
+    });
+
+    it('should set the boxShadow property of the element', function (){
         expect(element.style.boxShadow).toBe('0 0 15px rgba(81, 250, 200, 1)');
     });
 
     it('should set the border property of the element', function (){
-        element = { style: { border: '' } };
-        spyOn(element, 'style');
-        HIGHLIGHT.glow(element);
         expect(element.style.border).toBe('1px solid rgba(81, 250, 200, 1)');
     });
 });
